15adb52967a79907cbb3681d9aad5d9086bdd57d
nanna29 <luna2907@naver.com>
2023-12-13 00:47:36 +0900

    feat: Add message controller logic (#1)

--- backend/src/main/java/com/gdsc_solutionchallenge/backend/domain/result/controller/MessageController.java
+++ backend/src/main/java/com/gdsc_solutionchallenge/backend/domain/result/controller/MessageController.java
@@ -1,11 +1,15 @@
 package com.gdsc_solutionchallenge.backend.domain.result.controller;
 
+import com.gdsc_solutionchallenge.backend.domain.result.domain.Message;
+import com.gdsc_solutionchallenge.backend.domain.result.service.MessageService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
 
 @RestController
 @RequiredArgsConstructor
@@ -15,7 +19,9 @@ public class MessageController {
 
     // Json객체 -> Dto로 전환 후, service 단으로 이동
     @PostMapping("/message")
-    public ResponseEntity<Object> phishingMessage(@RequestBody MessageRequestDto messageRequestDto){
-        return messageService.isPhishingMessage(messageRequestDto);
+    public List<Message> phishingMessage(/*@RequestBody MessageRequestDto messageRequestDto*/){
+        //return messageService.isPhishingMessage(messageRequestDto);
+        List<Message> message=messageService.getMessage();
+        return message;
     }
 }
---
+++ backend/src/main/java/com/gdsc_solutionchallenge/backend/domain/result/service/MessageService.java
@@ -0,0 +1,13 @@
+package com.gdsc_solutionchallenge.backend.domain.result.service;
+
+import com.gdsc_solutionchallenge.backend.domain.result.domain.Message;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+
+@RequiredArgsConstructor
+@Service
+public class MessageService {
+    public Message getMessage(){
+
+    }
+}
