e5afafd723f6a2e437b775d6bdc886e07f48ef67
nanna29 <luna2907@naver.com>
2023-12-12 23:55:39 +0900

    feat: Add firebaseConfig initialize method (#1)

---
+++ backend/src/main/java/com/gdsc_solutionchallenge/backend/domain/result/config/FirebaseConfig.java
@@ -0,0 +1,30 @@
+package com.gdsc_solutionchallenge.backend.domain.result.config;
+
+import com.google.auth.oauth2.GoogleCredentials;
+import com.google.firebase.FirebaseApp;
+import com.google.firebase.FirebaseOptions;
+import jakarta.annotation.PostConstruct;
+import org.springframework.context.annotation.Configuration;
+
+import java.io.FileInputStream;
+
+@Configuration
+
+public class FirebaseConfig {
+    @PostConstruct
+    public void initialize(){
+        try {
+            FileInputStream serviceAccount =
+                    new FileInputStream("src/main/resources/serviceAccountKey.json");
+
+            FirebaseOptions options = new FirebaseOptions.Builder()
+                    .setCredentials(GoogleCredentials.fromStream(serviceAccount))
+                    //.setDatabaseUrl("https://{{데이터베이스명}}.firebaseio.com")
+                    .build();
+
+            FirebaseApp.initializeApp(options);
+        } catch (Exception e){
+            e.printStackTrace();
+        }
+    }
+}
