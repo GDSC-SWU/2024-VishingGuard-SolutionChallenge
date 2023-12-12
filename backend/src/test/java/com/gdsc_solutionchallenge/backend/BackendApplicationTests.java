2e874894dfc417f4f6f505f59ed68d1afb584861
nanna29 <luna2907@naver.com>
2023-12-12 21:15:25 +0900

    chore: initialize Spring Boot project (#1)

---
+++ backend/src/test/java/com/gdsc_solutionchallenge/backend/BackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.gdsc_solutionchallenge.backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
