54bda0cd25f336f597b7e1f11177c1c39c16b488
nanna29 <luna2907@naver.com>
2023-12-13 01:30:16 +0900

    test: Add unit test for MessageController (#1)

--- backend/src/test/java/com/gdsc_solutionchallenge/backend/domain/result/controller/MessageControllerTest.java
+++ backend/src/test/java/com/gdsc_solutionchallenge/backend/domain/result/controller/MessageControllerTest.java
@@ -1,7 +1,34 @@
 package com.gdsc_solutionchallenge.backend.domain.result.controller;
 
+import com.gdsc_solutionchallenge.backend.domain.result.domain.Message;
+import com.google.cloud.firestore.DocumentReference;
+import com.google.cloud.firestore.Firestore;
+import com.google.firebase.cloud.FirestoreClient;
+import org.junit.jupiter.api.Test;
+
+import java.util.HashMap;
+import java.util.Map;
+
 import static org.junit.jupiter.api.Assertions.*;
 
 class MessageControllerTest {
+    @Test
+    public void saveMessageTest() throws Exception{
+        Message message = Message.builder()
+                .messageKeyword("Test content")
+                .build();
+
+        // Firestore 인스턴스 가져오기
+        Firestore firestore = FirestoreClient.getFirestore();
+
+        // Firestore에 데이터 추가
+        Map<String, Object> messageData = new HashMap<>();
+        messageData.put("messageKeyword", message.getMessageKeyword());
+
+        DocumentReference documentReference = firestore.collection("messages").add(messageData).get();
+        String generatedId = documentReference.getId();
+        System.out.println(generatedId);
+    }
+
 
 }
