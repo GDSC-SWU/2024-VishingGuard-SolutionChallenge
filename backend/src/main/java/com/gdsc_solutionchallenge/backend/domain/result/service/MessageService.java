24d78cf02936f7243b7e97ec183c59d3c45b5540
nanna29 <luna2907@naver.com>
2023-12-13 00:50:19 +0900

    feat: Add message service logic (#1)

--- backend/src/main/java/com/gdsc_solutionchallenge/backend/domain/result/service/MessageService.java
+++ backend/src/main/java/com/gdsc_solutionchallenge/backend/domain/result/service/MessageService.java
@@ -1,13 +1,29 @@
 package com.gdsc_solutionchallenge.backend.domain.result.service;
 
 import com.gdsc_solutionchallenge.backend.domain.result.domain.Message;
+import com.google.api.core.ApiFuture;
+import com.google.cloud.firestore.Firestore;
+import com.google.cloud.firestore.QueryDocumentSnapshot;
+import com.google.cloud.firestore.QuerySnapshot;
+import com.google.firebase.cloud.FirestoreClient;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
+
+import java.util.ArrayList;
+import java.util.List;
 
 @RequiredArgsConstructor
 @Service
 public class MessageService {
-    public Message getMessage(){
-
+    public static final String COLLECTION_NAME = "Message";
+    public List<Message> getMessage(){
+        List<Message> list = new ArrayList<>();
+        Firestore db = FirestoreClient.getFirestore();
+        ApiFuture<QuerySnapshot> future = db.collection(COLLECTION_NAME).get();
+        List<QueryDocumentSnapshot> documents = future.get().getDocuments();
+        for (QueryDocumentSnapshot document : documents) {
+            list.add(document.toObject(Message.class));
+        }
+        return list;
     }
 }
