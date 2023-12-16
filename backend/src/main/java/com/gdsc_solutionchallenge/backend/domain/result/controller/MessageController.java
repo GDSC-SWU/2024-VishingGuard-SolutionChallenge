b2f60dd28b5c225330205bdde377ff66f62961b9
nanna29 <luna2907@naver.com>
2023-12-17 00:12:55 +0900

    Chore: Change springfox to springdoc

--- backend/src/main/java/com/gdsc_solutionchallenge/backend/domain/result/controller/MessageController.java
+++ backend/src/main/java/com/gdsc_solutionchallenge/backend/domain/result/controller/MessageController.java
@@ -3,6 +3,11 @@ package com.gdsc_solutionchallenge.backend.domain.result.controller;
 import com.gdsc_solutionchallenge.backend.domain.result.domain.Message;
 import com.gdsc_solutionchallenge.backend.domain.result.dto.MessageRequestDto;
 import com.gdsc_solutionchallenge.backend.domain.result.service.MessageService;
+import io.swagger.annotations.Api;
+import io.swagger.annotations.ApiOperation;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.Parameter;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
@@ -13,11 +18,14 @@ import java.util.concurrent.ExecutionException;
 @RestController
 @RequiredArgsConstructor
 @RequestMapping("/api/v1/result")
+@Tag(name = "예제 API", description = "Swagger 테스트용 API")
 public class MessageController {
     private final MessageService messageService;
 
     // Json객체 -> Dto로 전환 후, service 단으로 이동
     @PostMapping("/message")
+    @Operation(summary = "문자열 반복", description = "파라미터로 받은 문자열을 2번 반복합니다.")
+    @Parameter(name = "str", description = "2번 반복할 문자열")
     public ResponseEntity<Object> phishingMessage(@RequestBody MessageRequestDto messageRequestDto){
         return messageService.isPhishingMessage(messageRequestDto);
     }
