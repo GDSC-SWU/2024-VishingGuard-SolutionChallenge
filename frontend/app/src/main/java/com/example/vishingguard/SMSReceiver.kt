da67a3ae51ffd8a70980fd32e2689af5c665dadb
youjin09222 <youjin09222@gmail.com>
2023-12-17 02:00:07 +0900

    feat: Add BroadcastReceiver (#2)

---
+++ frontend/app/src/main/java/com/example/vishingguard/SMSReceiver.kt
@@ -0,0 +1,10 @@
+package com.example.vishingguard
+
+import android.content.BroadcastReceiver
+import android.content.Context
+import android.content.Intent
+
+class SMSReceiver : BroadcastReceiver() {
+    override fun onReceive(context: Context, intent: Intent) {
+    }
+}
