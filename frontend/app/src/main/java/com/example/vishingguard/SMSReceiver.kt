5097a0a63d04ed71c4db9f5e2be4478d09186c8a
youjin09222 <youjin09222@gmail.com>
2023-12-17 02:03:53 +0900

    feat: Add message extraction (#2)

--- frontend/app/src/main/java/com/example/vishingguard/SMSReceiver.kt
+++ frontend/app/src/main/java/com/example/vishingguard/SMSReceiver.kt
@@ -3,8 +3,34 @@ package com.example.vishingguard
 import android.content.BroadcastReceiver
 import android.content.Context
 import android.content.Intent
+import android.os.Bundle
+import android.telephony.SmsMessage
+import android.util.Log
 
 class SMSReceiver : BroadcastReceiver() {
-    override fun onReceive(context: Context, intent: Intent) {
+
+    // 문자가 왔을 때 실행
+    override fun onReceive(context: Context?, intent: Intent?) {
+        if(intent?.action.equals("android.provider.Telephony.SMS_RECEIVED")){
+            val bundle = intent?.extras
+            val messages = MessageParse(bundle!!)
+
+            if(messages?.size!! > 0){
+                val content = messages[0]?.messageBody.toString()
+                if (content != null) {
+                    Log.d("문자 내용 추출", content)
+                }
+            }
+        }
+    }
+
+    // 문자 내용 추출
+    fun MessageParse(bundle: Bundle): Array<SmsMessage?>? {
+        val objs = bundle["pdus"] as Array<Any>?
+        val messages: Array<SmsMessage?> = arrayOfNulls<SmsMessage>(objs!!.size)
+        for (i in objs!!.indices) {
+            messages[i] = SmsMessage.createFromPdu(objs[i] as ByteArray)
+        }
+        return messages
     }
 }
