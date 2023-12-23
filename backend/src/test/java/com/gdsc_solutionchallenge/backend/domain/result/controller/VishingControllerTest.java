83e145d7d740b505451ce81cd1b2b0814a5982ff
nanna29 <luna2907@naver.com>
2023-12-23 13:25:54 +0900

    teat: Add VishingControllerTest code (#4)

--- backend/src/test/java/com/gdsc_solutionchallenge/backend/domain/result/controller/VishingControllerTest.java
+++ backend/src/test/java/com/gdsc_solutionchallenge/backend/domain/result/controller/VishingControllerTest.java
@@ -2,8 +2,10 @@ package com.gdsc_solutionchallenge.backend.domain.result.controller;
 
 import com.fasterxml.jackson.databind.ObjectMapper;
 import com.gdsc_solutionchallenge.backend.domain.result.domain.SmishingRepository;
+import com.gdsc_solutionchallenge.backend.domain.result.domain.Vishing;
+import com.gdsc_solutionchallenge.backend.domain.result.domain.VishingRepository;
 import com.gdsc_solutionchallenge.backend.domain.result.dto.SmishingRequestDto;
-
+import com.gdsc_solutionchallenge.backend.domain.result.dto.VishingRequestDto;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
@@ -14,38 +16,38 @@ import org.springframework.boot.test.web.server.LocalServerPort;
 import org.springframework.test.context.junit.jupiter.SpringExtension;
 import org.springframework.test.web.servlet.MockMvc;
 
+import static org.junit.jupiter.api.Assertions.*;
 import static org.springframework.http.MediaType.APPLICATION_JSON;
-
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
 import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
 @ExtendWith(SpringExtension.class)
 @SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
 @AutoConfigureMockMvc(addFilters = false)
-//@Transactional
-class SmishingControllerTest {
+class VishingControllerTest {
     @LocalServerPort
     private int port;
     @Autowired
-    private SmishingRepository smishingRepository;
+    private VishingRepository vishingRepository;
     @Autowired
     private MockMvc mockMvc;
 
     @Test
-    @DisplayName("스미싱 검사 테스트")
+    @DisplayName("보이스 피싱 검사 테스트")
     public void testMessagePhishing() throws Exception {
         // Given
-        /*Message message1 = messageRepository.saveMessage(Message.builder()
-                .messageKeyword("엄마")
+        Vishing vishing1 = vishingRepository.saveVishing(Vishing.builder()
+                .vishingKeyword("도용")
                 .build());
 
-        Message message2 = messageRepository.saveMessage(Message.builder()
-                .messageKeyword("기프트 카드")
-                .build());*/
+        Vishing vishing2 = vishingRepository.saveVishing(Vishing.builder()
+                .vishingKeyword("개인정보유출")
+                .build());
 
-        SmishingRequestDto smishingRequestDto = SmishingRequestDto.builder()
-                .smishingScript("대포통장에 기프트 카드")
+        VishingRequestDto vishingRequestDto = VishingRequestDto.builder()
+                .vishingScript("고객님의 명의가 도용되었습니다")
                 .build();
 
         // When
@@ -54,7 +56,7 @@ class SmishingControllerTest {
         // Then
         mockMvc.perform(post(url)
                         .contentType(APPLICATION_JSON)
-                        .content(new ObjectMapper().writeValueAsString(smishingRequestDto)))
+                        .content(new ObjectMapper().writeValueAsString(vishingRequestDto)))
                 .andExpect(status().isOk())
                 .andExpect(jsonPath("$").isBoolean())
                 .andDo(print());
