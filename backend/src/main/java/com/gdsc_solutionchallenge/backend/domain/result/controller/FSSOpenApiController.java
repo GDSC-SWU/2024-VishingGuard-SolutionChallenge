fcf86d9ae1a5804d228235f8b0d2832a33e93b26
nanna29 <luna2907@naver.com>
2024-01-02 00:51:37 +0900

    refactor: Modify to Integrate open api calls and file download url calls (#3)

--- backend/src/main/java/com/gdsc_solutionchallenge/backend/domain/result/controller/FSSOpenApiController.java
+++ backend/src/main/java/com/gdsc_solutionchallenge/backend/domain/result/controller/FSSOpenApiController.java
@@ -1,20 +1,27 @@
 package com.gdsc_solutionchallenge.backend.domain.result.controller;
 
+import com.gdsc_solutionchallenge.backend.domain.result.common.BaseResponse;
 import com.gdsc_solutionchallenge.backend.domain.result.dto.OpenApiDto;
 import com.gdsc_solutionchallenge.backend.domain.result.service.FSSOpenApiService;
 import com.gdsc_solutionchallenge.backend.domain.result.service.SmishingService;
 import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.Parameter;
 import io.swagger.v3.oas.annotations.tags.Tag;
 import lombok.RequiredArgsConstructor;
+import org.springframework.core.io.InputStreamResource;
+import org.springframework.core.io.Resource;
+import org.springframework.http.ContentDisposition;
+import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 import org.springframework.web.client.RestTemplate;
 import org.springframework.beans.factory.annotation.Value;
 
+import java.io.File;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
 import java.util.List;
 
 @RestController
@@ -23,14 +30,11 @@ import java.util.List;
 @Tag(name = "금감원 API", description = "금감원 open api 호출 API")
 public class FSSOpenApiController {
     private final FSSOpenApiService fssOpenApiService;
-
-    // 파일 다운로드 url 담아놓을 배열
-    String[] urls;
     @Value("${fss.api.url}")
     private String apiUrl;
-    @GetMapping("/financial")
-    @Operation(summary = "금감원 API", description = "금감원 API")
-    public ResponseEntity<Object> getFssApi() {
+    @GetMapping("/financial/download/{number}")
+    @Operation(summary = "금감원 API", description = "금감원 API 통신 및 파일 다운로드")
+    public ResponseEntity<Object> downloadFile(@PathVariable("number") int number){
         try {
             // RestTemplate을 사용하여 GET 요청 보내기
             RestTemplate restTemplate = new RestTemplate();
@@ -38,17 +42,46 @@ public class FSSOpenApiController {
             // 응답받은 data parsing 진행
             String parsedData = fssOpenApiService.dataParsing(responseData);
             // | 기준으로 분리
-            urls=parsedData.split("\\|");
-            // 성공적인 응답을 클라이언트에 반환
-            return ResponseEntity.ok(urls);
+            String[] urls=parsedData.split("\\|");
+
+            // 파일 다운로드
+            if (urls != null && number >= 0 && number < urls.length) {
+                String fileUrl = urls[number];
+
+                // 파일의 URL을 반환
+                return ResponseEntity
+                        .status(HttpStatus.OK)
+                        .body(new BaseResponse<>(HttpStatus.OK.value(), "다운로드 URL입니다", fileUrl));
+
+            } else {
+                return ResponseEntity.status(HttpStatus.NOT_FOUND).body(urls[0]);
+            }
         } catch (Exception e) {
-            // 오류 발생 시, 클라이언트에게 500 Internal Server Error와 함께 예외 메시지 반환
             return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).body(e.getMessage());
         }
     }
 
-    @GetMapping("/financial/download/{number}")
-    public ResponseEntity<Object> downloadFile(@PathVariable int num){
+    /*@GetMapping("/financial/download/{number}")
+    @Operation(summary = "금감원 파일 다운로드", description = "금감원 파일 다운로드")
+    @Parameter(name = "파일 번호", description = "다운로드 할 파일 번호")
+    public ResponseEntity<Object> downloadFile(@PathVariable("number") int number){
+        try {
+            Object result = fssOpenApiService.downloadFile(urls[number]);
+            if (urls != null && number >= 0 && number < urls.length) {
+                String fileUrl = urls[number];
 
-    }
+                Path filePath = Paths.get(fileUrl);
+                Resource resource = new InputStreamResource(Files.newInputStream(filePath)); // 파일 resource 얻기
+
+                return ResponseEntity
+                        .status(HttpStatus.OK)
+                        .body(new BaseResponse<>(HttpStatus.OK.value(), "피싱 결과입니다", resource));
+            } else {
+                return ResponseEntity.status(HttpStatus.NOT_FOUND).body(urls[0]);
+            }
+        } catch (Exception e) {
+            return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).body(e.getMessage());
+        }
+
+    }*/
 }
