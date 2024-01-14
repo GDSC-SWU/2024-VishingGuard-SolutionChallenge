485d5c886da7ba0e326299289b0e3c34bffde143
Nellie <kny@swu.ac.kr>
2024-01-14 19:24:13 +0900

    Revert "[BE] URL 피싱 판단 & 스팸 전화번호 확인"

--- frontend/app/src/main/java/com/example/vishingguard/SmishingResponse.kt
+++
@@ -1,9 +0,0 @@
-package com.example.vishingguard
-
-import kotlinx.serialization.SerialName
-import kotlinx.serialization.Serializable
-@Serializable
-data class SmishingResponse(
-    @SerialName("smishing")
-    val smishing: Boolean
-)
