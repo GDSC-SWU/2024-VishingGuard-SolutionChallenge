1f6241a356c547e66a082d97d0be2e0c2e2be2b6
nanna29 <luna2907@naver.com>
2024-01-14 16:58:25 +0900

    feat: Add SpamnumService (#9)

---
+++ backend/src/main/java/com/gdsc_solutionchallenge/backend/domain/result/service/SpamNumService.java
@@ -0,0 +1,36 @@
+package com.gdsc_solutionchallenge.backend.domain.result.service;
+
+import com.gdsc_solutionchallenge.backend.domain.result.domain.Smishing;
+import com.gdsc_solutionchallenge.backend.domain.result.domain.SmishingRepository;
+import com.gdsc_solutionchallenge.backend.domain.result.domain.SpamNumber;
+import com.gdsc_solutionchallenge.backend.domain.result.domain.SpamNumberRepository;
+import com.gdsc_solutionchallenge.backend.domain.result.dto.SmishingRequestDto;
+import com.gdsc_solutionchallenge.backend.domain.result.dto.SpamNumRequsetDto;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@RequiredArgsConstructor
+@Service
+public class SpamNumService {
+    public final SpamNumberRepository spamNumberRepository;
+
+    public boolean isSpamNum(SpamNumRequsetDto spamNumRequsetDto) throws Exception {
+        List<SpamNumber> spamNumberList = spamNumberRepository.getAllNumbers();
+
+        for (SpamNumber spamNumber : spamNumberList){
+            if (spamNumber != null && removeHyphens(spamNumRequsetDto.getSpamNumber())
+                    .contains(removeHyphens(spamNumber.getNumber()))) {
+                return true;
+            }
+        }
+        return false;
+    }
+
+    // 하이픈 삭제
+    private String removeHyphens(String input) {
+        return input.replaceAll("-", "");
+    }
+
+}
