8cc738d817b9e7ba8fdeb3330c52e94aacafac91
youjin09222 <youjin09222@gmail.com>
2024-01-20 01:44:55 +0900

    feat: Add Fragment (#17)

---
+++ frontend/app/src/main/java/com/example/vishingguard/pishing/PishingFragment.kt
@@ -0,0 +1,10 @@
+package com.example.vishingguard.pishing
+
+import com.example.vishingguard.R
+import com.example.vishingguard.base.BindingFragment
+import com.example.vishingguard.databinding.FragmentPishingBinding
+
+class PishingFragment : BindingFragment<FragmentPishingBinding>(R.layout.fragment_pishing) {
+    override fun initView() {
+    }
+}
