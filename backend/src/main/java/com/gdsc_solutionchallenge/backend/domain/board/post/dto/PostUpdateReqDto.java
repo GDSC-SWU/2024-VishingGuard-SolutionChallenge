d6fbf0345c7dabd973a479a08ef8f5ab0cb9e0bd
nanna29 <luna2907@naver.com>
2024-01-25 00:10:34 +0900

    feat: Add posting rules in  PostUpdateReqDto (#16)

--- backend/src/main/java/com/gdsc_solutionchallenge/backend/domain/board/post/dto/PostUpdateReqDto.java
+++ backend/src/main/java/com/gdsc_solutionchallenge/backend/domain/board/post/dto/PostUpdateReqDto.java
@@ -1,10 +1,12 @@
 package com.gdsc_solutionchallenge.backend.domain.board.post.dto;
 
+import com.gdsc_solutionchallenge.backend.global.error.BaseException;
 import io.swagger.v3.oas.annotations.media.Schema;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
+import org.springframework.http.HttpStatus;
 
 @Getter
 @NoArgsConstructor
@@ -18,5 +20,26 @@ public class PostUpdateReqDto {
     public PostUpdateReqDto(String title, String content){
         this.title=title;
         this.content=content;
+    }
+
+    // 게시글 제목 관련 설정 메서드
+    // 제목이 공백이거나 50자 이상을 초과할 경우 BaseException 발생됨
+    public void setTitle(String title) {
+        if (title == null || title.trim().isEmpty()) {
+            throw new BaseException(HttpStatus.BAD_REQUEST.value(), "게시판 제목은 공백일 수 없습니다.");
+        }
+        if (title.length() > 50) {
+            throw new BaseException(HttpStatus.BAD_REQUEST.value(),"게시판 제목은 " + 50 + "자 이하로 입력해야 합니다.");
+        }
+        this.title = title;
+    }
+
+    // 게시글 내용 관련 설정 메서드
+    // 내용이 공백일 경우 BaseException 발생됨
+    public void setContent(String content) {
+        if (content == null || content.trim().isEmpty()) {
+            throw new BaseException(HttpStatus.BAD_REQUEST.value(), "게시판 내용은 공백일 수 없습니다.");
+        }
+        this.content = content;
     }
 }
