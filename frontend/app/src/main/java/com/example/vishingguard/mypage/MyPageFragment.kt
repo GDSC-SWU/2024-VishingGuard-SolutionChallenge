b82989b48f336cbbf2f641ef537caab65bdef2ce
youjin09222 <youjin09222@gmail.com>
2024-01-31 23:52:53 +0900

    feat: Add navigateToEditFragment from MyPageFragment (#21)

--- frontend/app/src/main/java/com/example/vishingguard/mypage/MyPageFragment.kt
+++ frontend/app/src/main/java/com/example/vishingguard/mypage/MyPageFragment.kt
@@ -1,10 +1,29 @@
 package com.example.vishingguard.mypage
 
+import androidx.fragment.app.FragmentManager
+import androidx.fragment.app.FragmentTransaction
+import androidx.navigation.ui.NavigationUI.setupWithNavController
 import com.example.vishingguard.R
 import com.example.vishingguard.base.BindingFragment
 import com.example.vishingguard.databinding.FragmentMyPageBinding
 
 class MyPageFragment : BindingFragment<FragmentMyPageBinding>(R.layout.fragment_my_page) {
     override fun initView() {
+
+        // 정보 수정 화면으로 이동
+        binding.btnEdit.setOnClickListener {
+            navigateToEditFragment()
+        }
+    }
+
+    // EditFragment로 이동
+    private fun navigateToEditFragment() {
+        val editFragment = EditFragment()
+        val fragmentManager = requireActivity().supportFragmentManager
+        val transaction = fragmentManager.beginTransaction()
+
+        transaction.replace(R.id.container_main, editFragment)
+        transaction.addToBackStack(null)
+        transaction.commit()
     }
 }
