94929789b13f217ceddc23cd53320ffb104fae6b
youjin09222 <youjin09222@gmail.com>
2024-01-31 23:50:45 +0900

    ui: Add EditFragment UI (#21)

---
+++ frontend/app/src/main/java/com/example/vishingguard/mypage/EditFragment.kt
@@ -0,0 +1,10 @@
+package com.example.vishingguard.mypage
+
+import com.example.vishingguard.R
+import com.example.vishingguard.base.BindingFragment
+import com.example.vishingguard.databinding.FragmentMyPageBinding
+
+class EditFragment : BindingFragment<FragmentMyPageBinding>(R.layout.fragment_edit) {
+    override fun initView() {
+    }
+}
