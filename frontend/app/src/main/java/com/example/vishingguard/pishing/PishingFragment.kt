ebb7902cbe3e3e88d7588dfe65e7357070d11ecc
youjin09222 <youjin09222@gmail.com>
2024-02-12 23:00:19 +0900

    feat: Add postVishing in PishingFragment (#36)

--- frontend/app/src/main/java/com/example/vishingguard/pishing/PishingFragment.kt
+++ frontend/app/src/main/java/com/example/vishingguard/pishing/PishingFragment.kt
@@ -1,10 +1,44 @@
 package com.example.vishingguard.pishing
 
+import android.view.View
+import androidx.core.view.isVisible
+import androidx.fragment.app.viewModels
 import com.example.vishingguard.R
 import com.example.vishingguard.base.BindingFragment
 import com.example.vishingguard.databinding.FragmentPishingBinding
+import com.example.vishingguard.pishing.data.ReportViewModel
+import com.example.vishingguard.pishing.data.VishingReportAdapter
 
 class PishingFragment : BindingFragment<FragmentPishingBinding>(R.layout.fragment_pishing) {
+
+    private val viewModel by viewModels<ReportViewModel>()
     override fun initView() {
+        // Request to post reports to the server
+        viewModel.postVishing()
+
+        // Handle the response from ViewModel
+        handleReportResponse()
+    }
+
+    private fun handleReportResponse() {
+        // Observe Vishing data
+        viewModel.postVishing.observe(viewLifecycleOwner) { response ->
+            if (response.data.isNotEmpty()) {
+                // populate RecyclerView with Vishing reports
+                val data = response.data
+                val adapter = VishingReportAdapter(data)
+                binding.rvVishing.adapter = adapter
+            } else {
+                // No Vishing Data views
+                showNoDataViews(binding.bgCall, binding.imgCall, binding.tvResultCall)
+            }
+        }
+    }
+
+    // Show views indicating no data available
+    private fun showNoDataViews(bgView: View, imageView: View, textView: View) {
+        bgView.isVisible = true
+        imageView.isVisible = true
+        textView.isVisible = true
     }
 }
