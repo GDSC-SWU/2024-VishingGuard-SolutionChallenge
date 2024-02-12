b32a5addc6e8f42adc82d317a2014caf18cf1d8f
youjin09222 <youjin09222@gmail.com>
2024-02-12 23:12:26 +0900

    feat: Add UI update for phishing result display (#36)

--- frontend/app/src/main/java/com/example/vishingguard/pishing/PishingFragment.kt
+++ frontend/app/src/main/java/com/example/vishingguard/pishing/PishingFragment.kt
@@ -17,6 +17,7 @@ class PishingFragment : BindingFragment<FragmentPishingBinding>(R.layout.fragmen
         // Request to post reports to the server
         viewModel.postVishing()
         viewModel.postSmishing()
+        viewModel.getState()
 
         // Handle the response from ViewModel
         handleReportResponse()
@@ -48,6 +49,13 @@ class PishingFragment : BindingFragment<FragmentPishingBinding>(R.layout.fragmen
                 showNoDataViews(binding.bgMessage, binding.imgMessage, binding.tvResultMassage)
             }
         }
+
+        // Observe State data
+        viewModel.getState.observe(viewLifecycleOwner) { response ->
+            if (response.data.isNotEmpty()) {
+                setPhishingResult(response.data)
+            }
+        }
     }
 
     // Show views indicating no data available
@@ -55,5 +63,40 @@ class PishingFragment : BindingFragment<FragmentPishingBinding>(R.layout.fragmen
         bgView.isVisible = true
         imageView.isVisible = true
         textView.isVisible = true
+    }
+
+    // Set phishing result based on the response data
+    private fun setPhishingResult(state: String) {
+
+        when (state) {
+            "Risky" -> {
+                setResultUI(
+                    R.string.tv_title_risky, R.string.tv_result_risky, R.string.tv_content_risky,
+                    R.drawable.ic_step_risky, R.drawable.ic_frame_risky, R.drawable.img_result_risky
+                )
+            }
+            "Moderate" -> {
+                setResultUI(
+                    R.string.tv_title_moderate, R.string.tv_result_moderate, R.string.tv_content_moderate,
+                    R.drawable.ic_step_moderate, R.drawable.ic_frame_moderate, R.drawable.img_result_moderate
+                )
+            }
+            "Safe" -> {
+                setResultUI(
+                    R.string.tv_title_safe, R.string.tv_result_safe, R.string.tv_content_safe,
+                    R.drawable.ic_step_safe, R.drawable.ic_frame_safe,R.drawable.img_result_safe
+                )
+            }
+        }
+    }
+
+    // Set phishing result UI components
+    private fun setResultUI(titleResId: Int, resultResId: Int, contentResId: Int, stepResId: Int, frameResId: Int, resultImageResId: Int) {
+        binding.tvTitleResult.text = context?.getString(titleResId)
+        binding.tvResult.text = context?.getString(resultResId)
+        binding.tvResultContent.text = context?.getString(contentResId)
+        binding.imgStepSafe.setImageResource(stepResId)
+        binding.imgFrameSafe.setImageResource(frameResId)
+        binding.imgResult.setImageResource(resultImageResId)
     }
 }
