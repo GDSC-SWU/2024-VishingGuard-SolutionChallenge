3b58c8e9d7d5b0ac8eb31bd914d2d7d1e03fec8c
youjin09222 <youjin09222@gmail.com>
2024-02-14 02:30:19 +0900

    feat: Add getReport in HomeApiService (#41)

--- frontend/app/src/main/java/com/example/vishingguard/home/data/HomeApiService.kt
+++ frontend/app/src/main/java/com/example/vishingguard/home/data/HomeApiService.kt
@@ -1,6 +1,7 @@
 package com.example.vishingguard.home.data
 
 import com.example.vishingguard.home.procedure.data.ProcedureResponse
+import com.example.vishingguard.home.report.data.ReportResponse
 import retrofit2.Call
 import retrofit2.http.GET
 import retrofit2.http.Header
@@ -11,4 +12,9 @@ interface HomeApiService {
     fun getProcedure(
         @Header("Authorization") authorization: String
     ): Call<ProcedureResponse>
+
+    @GET("/api/v1/info/reportPlace")
+    fun getReport(
+        @Header("Authorization") authorization: String
+    ): Call<ReportResponse>
 }
