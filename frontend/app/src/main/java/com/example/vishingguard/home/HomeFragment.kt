33233994663fde9d6593aa8d8675be1b1e48b480
youjin09222 <youjin09222@gmail.com>
2024-02-15 23:14:30 +0900

    feat: Add postHome in HomeFragment (#48)

--- frontend/app/src/main/java/com/example/vishingguard/home/HomeFragment.kt
+++ frontend/app/src/main/java/com/example/vishingguard/home/HomeFragment.kt
@@ -2,12 +2,15 @@ package com.example.vishingguard.home
 
 import android.view.View
 import androidx.fragment.app.Fragment
+import androidx.fragment.app.viewModels
 import androidx.navigation.findNavController
 import androidx.viewpager2.widget.ViewPager2
 import com.example.vishingguard.R
 import com.example.vishingguard.base.BindingFragment
 import com.example.vishingguard.databinding.FragmentHomeBinding
 import com.example.vishingguard.home.data.BannerAdapter
+import com.example.vishingguard.home.data.HomeAdapter
+import com.example.vishingguard.home.data.HomeViewModel
 import com.example.vishingguard.home.prevention.PreventionFragment
 import com.example.vishingguard.home.procedure.ProcedureFragment
 import com.example.vishingguard.home.report.ReportFragment
@@ -15,6 +18,8 @@ import com.example.vishingguard.home.spam.SpamCheckFragment
 import com.google.android.material.bottomnavigation.BottomNavigationView
 
 class HomeFragment : BindingFragment<FragmentHomeBinding>(R.layout.fragment_home) {
+
+    private val viewModel by viewModels<HomeViewModel>()
 
     override fun initView() {
         // Create a list of images for the banner
@@ -30,6 +35,12 @@ class HomeFragment : BindingFragment<FragmentHomeBinding>(R.layout.fragment_home
 
         // Set up dots indicator for the banner
         binding.dotsIndicator.setViewPager2(binding.viewPagerBanner)
+
+        // Request to fetch posts from the server
+        viewModel.postHome()
+
+        // Handle the response from ViewModel
+        handlePostsResponse()
 
         // Move to the phishing report screen
         binding.btnCheck.setOnClickListener {
@@ -58,6 +69,20 @@ class HomeFragment : BindingFragment<FragmentHomeBinding>(R.layout.fragment_home
     }
 
     // Navigate to the phishing report screen
+    private fun handlePostsResponse() {
+        // Observe Posts data
+        viewModel.postHome.observe(viewLifecycleOwner) { response ->
+            if (response.data.isNotEmpty()) {
+                // Populate RecyclerView with Vishing reports
+                val data = response.data
+                val adapter = HomeAdapter(data)
+                binding.rvPosts.adapter = adapter
+
+            }
+        }
+    }
+
+    // Move to the phishing report screen
     private fun navigateToMyPage() {
         val navController = requireActivity().findNavController(R.id.container_main)
         navController.navigate(R.id.navigation_pishing)
