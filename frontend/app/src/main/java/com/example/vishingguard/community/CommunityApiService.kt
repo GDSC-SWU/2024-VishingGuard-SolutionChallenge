07f7bd178264f75542c533f58eaf6424be354041
youjin09222 <youjin09222@gmail.com>
2024-02-17 02:20:58 +0900

    feat: Add getRead in CommunityApiService (#52)

--- frontend/app/src/main/java/com/example/vishingguard/community/CommunityApiService.kt
+++ frontend/app/src/main/java/com/example/vishingguard/community/CommunityApiService.kt
@@ -1,13 +1,22 @@
 package com.example.vishingguard.community
 
 import com.example.vishingguard.community.posts.PostsResponse
+import com.example.vishingguard.community.read.ReadResponse
 import retrofit2.Call
 import retrofit2.http.GET
 import retrofit2.http.Header
+import retrofit2.http.Path
 
 interface CommunityApiService {
     @GET("/api/v1/posts/read")
     fun getPosts(
         @Header("Authorization") authorization: String
     ): Call<PostsResponse>
+
+    @GET("/api/v1/posts/{userId}/{postId}/read")
+    fun getRead(
+        @Header("Authorization") authorization: String,
+        @Path("userId") userId: Int,
+        @Path("postId") postId: String
+    ): Call<ReadResponse>
 }
