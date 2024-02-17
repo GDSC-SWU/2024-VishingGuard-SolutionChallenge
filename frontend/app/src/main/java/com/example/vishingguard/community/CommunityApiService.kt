286865739917f8c1bdf4b6f1f2a48e153b16831b
youjin09222 <youjin09222@gmail.com>
2024-02-17 23:50:16 +0900

    feat: Add postCreate in CommunityApiService (#52)

--- frontend/app/src/main/java/com/example/vishingguard/community/CommunityApiService.kt
+++ frontend/app/src/main/java/com/example/vishingguard/community/CommunityApiService.kt
@@ -1,10 +1,14 @@
 package com.example.vishingguard.community
 
+import com.example.vishingguard.community.create.CreateRequest
+import com.example.vishingguard.community.create.CreateResponse
 import com.example.vishingguard.community.posts.PostsResponse
 import com.example.vishingguard.community.read.ReadResponse
 import retrofit2.Call
+import retrofit2.http.Body
 import retrofit2.http.GET
 import retrofit2.http.Header
+import retrofit2.http.POST
 import retrofit2.http.Path
 
 interface CommunityApiService {
@@ -19,4 +23,11 @@ interface CommunityApiService {
         @Path("userId") userId: Int,
         @Path("postId") postId: String
     ): Call<ReadResponse>
+
+    @POST("/api/v1/posts/{userId}/create")
+    fun postCreate(
+        @Header("Authorization") authorization: String,
+        @Path("userId") userId: Int,
+        @Body params: CreateRequest
+    ): Call<CreateResponse>
 }
