105ad8676eaa8ba90083d2c1ae0019c6a324a333
youjin09222 <youjin09222@gmail.com>
2024-02-17 23:58:05 +0900

    feat: Add deletePost in CommunityApiService (#52)

--- frontend/app/src/main/java/com/example/vishingguard/community/CommunityApiService.kt
+++ frontend/app/src/main/java/com/example/vishingguard/community/CommunityApiService.kt
@@ -2,10 +2,12 @@ package com.example.vishingguard.community
 
 import com.example.vishingguard.community.create.CreateRequest
 import com.example.vishingguard.community.create.CreateResponse
+import com.example.vishingguard.community.delete.DeleteResponse
 import com.example.vishingguard.community.posts.PostsResponse
 import com.example.vishingguard.community.read.ReadResponse
 import retrofit2.Call
 import retrofit2.http.Body
+import retrofit2.http.DELETE
 import retrofit2.http.GET
 import retrofit2.http.Header
 import retrofit2.http.POST
@@ -30,4 +32,11 @@ interface CommunityApiService {
         @Path("userId") userId: Int,
         @Body params: CreateRequest
     ): Call<CreateResponse>
+
+    @DELETE("/api/v1/posts/{userId}/{postId}/delete")
+    fun deletePost(
+        @Header("Authorization") authorization: String,
+        @Path("userId") userId: Int,
+        @Path("postId") postId: String
+    ): Call<DeleteResponse>
 }
