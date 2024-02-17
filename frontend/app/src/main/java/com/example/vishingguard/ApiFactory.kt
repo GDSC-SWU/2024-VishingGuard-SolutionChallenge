05cfe30b909b8069d32b1d39e8a89930b4f97e15
youjin09222 <youjin09222@gmail.com>
2024-02-17 23:59:15 +0900

    feat: Add deletePost in ApiFactory (#52)

--- frontend/app/src/main/java/com/example/vishingguard/ApiFactory.kt
+++ frontend/app/src/main/java/com/example/vishingguard/ApiFactory.kt
@@ -45,6 +45,7 @@ object ServicePool {
     val getPosts = ApiFactory.retrofit.create(CommunityApiService::class.java)
     val getRead = ApiFactory.retrofit.create(CommunityApiService::class.java)
     val postCreate = ApiFactory.retrofit.create(CommunityApiService::class.java)
+    val deletePost = ApiFactory.retrofit.create(CommunityApiService::class.java)
 
     // Map
     val postRoute = ApiFactory.retrofit.create(RouteApiService::class.java)
