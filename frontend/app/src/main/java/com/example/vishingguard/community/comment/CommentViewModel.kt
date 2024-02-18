09af603c843e5e710c884ee3ca546ce69a391100
youjin09222 <youjin09222@gmail.com>
2024-02-18 23:21:42 +0900

    feat: Add postComment in CommentViewModel (#54)

--- frontend/app/src/main/java/com/example/vishingguard/community/comment/CommentViewModel.kt
+++ frontend/app/src/main/java/com/example/vishingguard/community/comment/CommentViewModel.kt
@@ -6,6 +6,8 @@ import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
 import com.example.vishingguard.ServicePool
 import com.example.vishingguard.community.PostData
+import com.example.vishingguard.community.comment.create.CommentRequest
+import com.example.vishingguard.community.comment.create.CommentResponse
 import com.example.vishingguard.login.data.UserData
 import retrofit2.Call
 import retrofit2.Response
@@ -20,6 +22,10 @@ class CommentViewModel : ViewModel() {
     private val _getComment: MutableLiveData<AllCommentResponse> = MutableLiveData()  //read, write
     val getComment: LiveData<AllCommentResponse> = _getComment //read
     private val getCommentService = ServicePool.getComment
+
+    private val _postComment: MutableLiveData<CommentResponse> = MutableLiveData()  //read, write
+    val postComment: LiveData<CommentResponse> = _postComment //read
+    private val postCommentService = ServicePool.postComment
 
     // Server interaction
     fun getComment() {
@@ -37,6 +43,25 @@ class CommentViewModel : ViewModel() {
 
                 override fun onFailure(call: Call<AllCommentResponse>, t: Throwable) {
                     t.message?.let { Log.d("error getComment", it) } ?: "Failed server communication (no response)"
+                }
+            })
+        }
+    }
+
+    fun postComment(postComment: CommentRequest) {
+        if (accessToken != null && userId != null && postId != null) {
+            postCommentService.postComment(accessToken, userId, postId, postComment).enqueue(object : retrofit2.Callback<CommentResponse> {
+                override fun onResponse(call: Call<CommentResponse>, response: Response<CommentResponse>) {
+                    if (response.isSuccessful) {
+                        _postComment.value = response?.body()
+                        Log.d("success postComment", _postComment.value.toString())
+                    } else {
+                        Log.d("error postComment", "Failed response")
+                    }
+                }
+
+                override fun onFailure(call: Call<CommentResponse>, t: Throwable) {
+                    t.message?.let { Log.d("error postComment", it) } ?: "Failed server communication (no response)"
                 }
             })
         }
