4997b627c5e548e44e34f0f1d4b78f4ae4d92d5e
youjin09222 <youjin09222@gmail.com>
2024-02-18 23:36:11 +0900

    feat: Add deleteComment in ApiFactory (#54)

--- frontend/app/src/main/java/com/example/vishingguard/ApiFactory.kt
+++ frontend/app/src/main/java/com/example/vishingguard/ApiFactory.kt
@@ -52,6 +52,7 @@ object ServicePool {
     val getComment = ApiFactory.retrofit.create(CommunityApiService::class.java)
     val postComment = ApiFactory.retrofit.create(CommunityApiService::class.java)
     val patchComment = ApiFactory.retrofit.create(CommunityApiService::class.java)
+    val deleteComment = ApiFactory.retrofit.create(CommunityApiService::class.java)
 
     // Map
     val postRoute = ApiFactory.retrofit.create(RouteApiService::class.java)
