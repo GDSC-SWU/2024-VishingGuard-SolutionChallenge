c99f5b75c621b6b445bc8fdaa2f68811f0b277c7
youjin09222 <youjin09222@gmail.com>
2024-02-18 23:37:07 +0900

    feat: Add deleteComment in UpdateCommentViewModel (#54)

--- frontend/app/src/main/java/com/example/vishingguard/community/comment/UpdateCommentViewModel.kt
+++ frontend/app/src/main/java/com/example/vishingguard/community/comment/UpdateCommentViewModel.kt
@@ -8,6 +8,7 @@ import com.example.vishingguard.ServicePool
 import com.example.vishingguard.community.PostData
 import com.example.vishingguard.community.comment.create.CommentRequest
 import com.example.vishingguard.community.comment.create.CommentResponse
+import com.example.vishingguard.community.comment.create.DelCommentResponse
 import com.example.vishingguard.login.data.UserData
 import retrofit2.Call
 import retrofit2.Response
@@ -19,10 +20,15 @@ class UpdateCommentViewModel : ViewModel() {
     val postId = PostData.getPostId()
     val commentId = PostData.getCommentId()
 
-    // UpdateComment LiveData
+    // Update Comment LiveData
     private val _patchComment: MutableLiveData<CommentResponse> = MutableLiveData()  //read, write
     val patchComment: LiveData<CommentResponse> = _patchComment //read
     private val patchCommentService = ServicePool.patchComment
+
+    // Delete Comment LiveData
+    private val _deleteComment: MutableLiveData<DelCommentResponse> = MutableLiveData()  //read, write
+    val deleteComment: LiveData<DelCommentResponse> = _deleteComment //read
+    private val deleteCommentService = ServicePool.deleteComment
 
     // Server interaction
     fun patchComment(postComment: CommentRequest) {
@@ -39,6 +45,25 @@ class UpdateCommentViewModel : ViewModel() {
 
                 override fun onFailure(call: Call<CommentResponse>, t: Throwable) {
                     t.message?.let { Log.d("error patchComment", it) } ?: "Failed server communication (no response)"
+                }
+            })
+        }
+    }
+
+    fun deleteComment() {
+        if (accessToken != null && userId != null && postId != null && commentId != null) {
+            deleteCommentService.deleteComment(accessToken, userId, postId, commentId).enqueue(object : retrofit2.Callback<DelCommentResponse> {
+                override fun onResponse(call: Call<DelCommentResponse>, response: Response<DelCommentResponse>) {
+                    if (response.isSuccessful) {
+                        _deleteComment.value = response?.body()
+                        Log.d("success deleteComment", _deleteComment.value.toString())
+                    } else {
+                        Log.d("error deleteComment", "Failed response")
+                    }
+                }
+
+                override fun onFailure(call: Call<DelCommentResponse>, t: Throwable) {
+                    t.message?.let { Log.d("error deleteComment", it) } ?: "Failed server communication (no response)"
                 }
             })
         }
