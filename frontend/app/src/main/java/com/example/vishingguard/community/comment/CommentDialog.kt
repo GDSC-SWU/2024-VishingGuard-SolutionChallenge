c8881cc72373c81d41decadd980da1142a3ddc40
youjin09222 <youjin09222@gmail.com>
2024-02-18 23:28:53 +0900

    feat: Add CommentDialog (#54)

---
+++ frontend/app/src/main/java/com/example/vishingguard/community/comment/CommentDialog.kt
@@ -0,0 +1,39 @@
+package com.example.vishingguard.community.comment
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import com.example.vishingguard.R
+import com.example.vishingguard.databinding.CommentDialogBinding
+import com.google.android.material.bottomsheet.BottomSheetDialogFragment
+
+class CommentDialog : BottomSheetDialogFragment() {
+
+    private var _binding: CommentDialogBinding? = null
+    private val binding get() = _binding!!
+    override fun getTheme(): Int {
+        return R.style.CustomBottomSheetDialogTheme
+    }
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View {
+        _binding = CommentDialogBinding.inflate(inflater, container, false)
+        val view = binding.root
+
+        binding.tvEdit.setOnClickListener {
+            val dialogFragment = UpdateCommentDialog()
+            fragmentManager?.let { it1 -> dialogFragment.show(it1, "Update CommentDialog") }
+            dismiss()
+        }
+
+        return view
+    }
+
+    override fun onDestroyView() {
+        super.onDestroyView()
+        _binding = null
+    }
+}
