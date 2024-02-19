bb02dcfb1f47e6cdad1c7706ec3a95f3c418c413
youjin09222 <youjin09222@gmail.com>
2024-02-19 23:44:49 +0900

    feat: Convert comments to English in MyPageFragment (#58)

--- frontend/app/src/main/java/com/example/vishingguard/mypage/MyPageFragment.kt
+++ frontend/app/src/main/java/com/example/vishingguard/mypage/MyPageFragment.kt
@@ -8,13 +8,13 @@ import com.example.vishingguard.databinding.FragmentMyPageBinding
 class MyPageFragment : BindingFragment<FragmentMyPageBinding>(R.layout.fragment_my_page) {
     override fun initView() {
 
-        // 정보 수정 화면으로 이동
+        // Update user information
         binding.btnUpdate.setOnClickListener {
             replaceFragment(UpdateFragment())
         }
     }
 
-    // 프래그먼트 이동
+    // Replace fragment
     fun replaceFragment(fragment: Fragment){
         requireActivity().supportFragmentManager.beginTransaction()
             .replace(R.id.container_main, fragment)
