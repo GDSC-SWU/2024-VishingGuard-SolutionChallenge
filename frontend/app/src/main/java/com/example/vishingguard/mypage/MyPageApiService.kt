115a68c92ca3850202115e430f1b3263090fe021
youjin09222 <youjin09222@gmail.com>
2024-02-19 23:41:46 +0900

    feat: Add updateUser in MyPageApiService (#58)

---
+++ frontend/app/src/main/java/com/example/vishingguard/mypage/MyPageApiService.kt
@@ -0,0 +1,18 @@
+package com.example.vishingguard.mypage
+
+import com.example.vishingguard.mypage.data.UpdateUserRequest
+import com.example.vishingguard.mypage.data.UserResponse
+import retrofit2.Call
+import retrofit2.http.Body
+import retrofit2.http.Header
+import retrofit2.http.POST
+import retrofit2.http.Path
+
+interface MyPageApiService {
+    @POST("/api/v1/myPage/{userId}/modify")
+    fun updateUser(
+        @Header("Authorization") authorization: String,
+        @Path("userId") userId: Int,
+        @Body params: UpdateUserRequest
+    ): Call<UserResponse>
+}
